3ec72e41837283bc5aca343cb06aebc4fe79a7f9
John Sseruwagi <sseruwagijohn95@gmail.com>
2023-04-05 19:33:34 +0300

    geting data from open weather api

--- Exercise Files/Ch01/01_04/begin/_scripts/ajax.js
+++ Exercise Files/Ch01/01_04/begin/_scripts/ajax.js
@@ -1,17 +1,20 @@
 // add global variable containing XHR object here
-
+var httpRequest = new XMLHttpRequest();
 
 // add get() function here
-
-
+const get = (url, success) => {
+	httpRequest.open("GET", url);
+	httpRequest.onload = () => success(httpRequest.responseText);
+	httpRequest.send();
+};
 function tempToF(kelvin) {
-    return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
+	return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
 }
 
 function successHandler(data) {
-    const dataObj = JSON.parse(data);
-    const weatherDiv = document.querySelector('#weather');
-    const weatherFragment = `
+	const dataObj = JSON.parse(data);
+	const weatherDiv = document.querySelector("#weather");
+	const weatherFragment = `
         <h1>Weather</h1>
         <h2 class="top">
         <img
@@ -22,16 +25,20 @@ function successHandler(data) {
         />${dataObj.name}
         </h2>
         <p>
-        <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | ${dataObj.weather[0].description}
+        <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | ${
+		dataObj.weather[0].description
+	}
         </p>
-    `
-    weatherDiv.innerHTML = weatherFragment;
-    weatherDiv.classList.remove('hidden');
+    `;
+	weatherDiv.innerHTML = weatherFragment;
+	weatherDiv.classList.remove("hidden");
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const apiKey = ''; // ADD YOUR API KEY BETWEEN THE QUOTES
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' + apiKey;
-    // add get() function call here
-    
+document.addEventListener("DOMContentLoaded", function () {
+	const apiKey = "db173f40ac169596678d02c3bb8882b2"; // ADD YOUR API KEY BETWEEN THE QUOTES
+	const url =
+		"https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=" +
+		apiKey;
+	// add get() function call here
+	get(url, successHandler);
 });
