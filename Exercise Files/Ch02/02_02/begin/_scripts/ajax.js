1b1647264439a27843edebcf9afad7d66a8b7726
John Sseruwagi <sseruwagijohn95@gmail.com>
2023-04-05 20:27:15 +0300

    make a function to return a promise

--- Exercise Files/Ch02/02_02/begin/_scripts/ajax.js
+++ Exercise Files/Ch02/02_02/begin/_scripts/ajax.js
@@ -1,20 +1,22 @@
-function get(url, success, fail) {
-    let httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET', url);
-    httpRequest.onload = function() {
-        if (httpRequest.status === 200) {
-            success(httpRequest.responseText);
-        } else {
-            fail(httpRequest.status);
-        }
-    }
-    httpRequest.send();
-};
+function get(url) {
+	return new Promise((resolve, reject) => {
+		let httpRequest = new XMLHttpRequest();
+		httpRequest.open("GET", url);
+		httpRequest.onload = function () {
+			if (httpRequest.status === 200) {
+				resolve(httpRequest.responseText);
+			} else {
+				reject(httpRequest.status);
+			}
+		};
+		httpRequest.send();
+	});
+}
 
 function successHandler(data) {
-    const dataObj = JSON.parse(data);
-    const weatherDiv = document.querySelector('#weather');
-    const weatherFragment = `
+	const dataObj = JSON.parse(data);
+	const weatherDiv = document.querySelector("#weather");
+	const weatherFragment = `
         <h1>Weather</h1>
         <h2 class="top">
         <img
@@ -25,26 +27,32 @@ function successHandler(data) {
         />${dataObj.name}
         </h2>
         <p>
-        <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | ${dataObj.weather[0].description}
+        <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | ${
+		dataObj.weather[0].description
+	}
         </p>
-    `
-    weatherDiv.innerHTML = weatherFragment;
-    weatherDiv.classList.remove('hidden');
+    `;
+	weatherDiv.innerHTML = weatherFragment;
+	weatherDiv.classList.remove("hidden");
 }
 
 function failHandler(status) {
-    console.log(status);
-    const weatherDiv = document.querySelector('#weather');
-    weatherDiv.classList.remove('hidden');
+	console.log(status);
+	const weatherDiv = document.querySelector("#weather");
+	weatherDiv.classList.remove("hidden");
 }
 
 function tempToF(kelvin) {
-    return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
+	return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const apiKey = ''; // ADD YOUR API KEY BETWEEN THE QUOTES
-//    const apiKey = '';
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' + apiKey;
-    get(url, successHandler, failHandler);
+document.addEventListener("DOMContentLoaded", function () {
+	const apiKey = ""; // ADD YOUR API KEY BETWEEN THE QUOTES
+	//    const apiKey = '';
+	const url =
+		"https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=" +
+		apiKey;
+	get(url)
+		.then((result) => successHandler(result))
+		.catch((error) => failHandler(error));
 });
